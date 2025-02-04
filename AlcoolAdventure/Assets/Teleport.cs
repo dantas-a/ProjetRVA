f11d103cf99139863d6bdd4a434d73d21896496e
Typical-Alex <alexandredant@hotmail.com>
2025-02-04 09:50:55 +0100

    MultipleThings

---
+++ AlcoolAdventure/Assets/Teleport.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] GameObject destination;
+    [SerializeField] GameObject playerg;
+
+    void OnTriggerEnter(Collider other) {
+    	if(other.CompareTag("Player")) {	
+    		playerg.SetActive(false);
+    		playerg.transform.position = destination.transform.position;
+    		playerg.SetActive(true);
+    	}
+    }
+}
