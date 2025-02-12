700ea56ab7d942feb9956e4ac0e79eff6ba3b202
Typical-Alex <alexandredant@hotmail.com>
2025-02-12 21:20:08 +0100

    Monter echelle

--- AlcoolAdventure/Assets/Scripts/ClimbLadder.cs
+++ AlcoolAdventure/Assets/Scripts/ClimbLadder.cs
@@ -1,16 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ClimbLadder : MonoBehaviour
 {  
-    void OnTriggerEnter(Collider other) {
-    	if(other.CompareTag("Player")) {
-    		//other.isClibbing = true;
-    	}
-    }
-    
-    void OnTriggerExit(Collider other) {
-    	if(other.CompareTag("Player")) {
-    		//other.isClibbing = false;
-    	}
-    }
+	private Transform chController;
+	bool inside = false;
+	public float speedUpDown = 3.2f;
+	private FirstPersonController FirstPersonController;
+	
+	void Start(){
+		FirstPersonController = GetComponent<FirstPersonController>();
+		chController = GetComponent<Transform>();
+		inside = false;
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Ladder") {
+			FirstPersonController.enabled = false;
+			inside = !inside;
+		}
+	}
+	
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.tag == "Ladder") {
+			FirstPersonController.enabled = true;
+			inside = !inside;
+		}
+	}
+	
+	void Update() {
+		if( inside == true && ( Input.GetKey("w")  || Input.GetKey("up")) ) {
+			chController.transform.position +=Vector3.up / speedUpDown;
+		}
+		if( inside == true && ( Input.GetKey("s")  || Input.GetKey("down")) ) {
+			chController.transform.position +=Vector3.down / speedUpDown;
+		}
+	}
 }
